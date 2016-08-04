1a7caf03efe071318c30ff441c233b9c1b709e69
Tofan Wahyu Tri Saputra <tofanwts@gmail.com>
2016-08-04 12:40:35 +0700

    Dashboard Page

---
+++ backend/pages/dashboard.php
@@ -0,0 +1,29 @@
+    <ol class="breadcrumb">
+        <li>
+            <i class="fa fa-dashboard"></i> Dashboard</a>
+        </li>
+    </ol>
+
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <div class="alert alert-success alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Selamat Datang Kembali!! \['-']/
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <p>
+                              <b>   <!-- fungsi sapa pelanggan -->
+                                <SCRIPT language=JavaScript>var d = new Date();
+                                             var h = d.getHours();
+                                             if (h < 11) { document.write('Selamat Pagi, Admin!'); }
+                                             else { if (h < 15) { document.write('Selamat Siang, Admin!'); }
+                                             else { if (h < 19) { document.write('Selamat Sore, Admin!'); }
+                                             else { if (h <= 23) { document.write('Selamat Malam, Admin!'); }
+                                             }}}
+                                </SCRIPT>
+                              </b>
+                            </p>
+                        </div>
+                    </div>
+                </div>
